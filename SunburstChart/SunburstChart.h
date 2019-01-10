b6a98d82c892377802e2f2fca30377856eb62b23
Oskar <oskar@cindori.org>
2019-01-10 20:29:35 +0100

    Initial Commit

---
+++ SunburstChart/SunburstChart.h
@@ -0,0 +1,19 @@
+//
+//  SunburstChart.h
+//  SunburstChart
+//
+//  Created by Oskar Groth on 2019-01-10.
+//  Copyright © 2019 Oskar Groth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SunburstChart.
+FOUNDATION_EXPORT double SunburstChartVersionNumber;
+
+//! Project version string for SunburstChart.
+FOUNDATION_EXPORT const unsigned char SunburstChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SunburstChart/PublicHeader.h>
+
+
