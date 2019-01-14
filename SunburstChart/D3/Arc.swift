3592db41eed02445082e2fdd2d23d1db94a0e8f5
Oskar <oskar@cindori.org>
2019-01-14 16:10:34 +0100

    something

---
+++ SunburstChart/D3/Arc.swift
@@ -0,0 +1,19 @@
+//
+//  Arc.swift
+//  SunburstChart
+//
+//  Created by Oskar Groth on 2019-01-14.
+//  Copyright © 2019 Oskar Groth. All rights reserved.
+//
+
+import Foundation
+
+struct Arc {
+    
+    var path: CGPath
+    
+    init(innerRadius: CGFloat, outerRadius: CGFloat, startAngle: CGFloat, endAngle: CGFloat) {
+        path.
+    }
+    
+}
